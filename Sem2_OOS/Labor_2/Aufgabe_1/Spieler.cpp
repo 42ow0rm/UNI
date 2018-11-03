f32c75a3929e96daa872e5a90129c5fafefc5091
42ow0rm <38980815+42ow0rm@users.noreply.github.com>
2018-11-03 13:34:28 +0100

    Update Spieler.cpp

--- Sem2_OOS/Labor_2/Aufgabe_1/Spieler.cpp
+++ Sem2_OOS/Labor_2/Aufgabe_1/Spieler.cpp
@@ -1,2 +1,21 @@
-
+#include <iostream>
+#include <conio.h>
 #include "Spieler.hpp"
+
+using namespace std;
+
+// Konstruktor
+Spieler::Spieler(MyString Name) {
+	
+
+}
+
+Spieler::Spieler(char * Name) {
+	
+	
+}
+
+Spieler::Spieler getPos(){
+	
+	
+}

d19ca8ea1151d0e7a98653118aff45b8fdccb326
42ow0rm <38980815+42ow0rm@users.noreply.github.com>
2018-11-03 13:32:15 +0100

    Update Labyrinth.cpp

--- Sem2_OOS/Labor_2/Aufgabe_1/Labyrinth.cpp
+++ Sem2_OOS/Labor_2/Aufgabe_1/Labyrinth.cpp
@@ -1,2 +1,53 @@
-
+#include <iostream>
+#include <conio.h> // für _getch()
+// Achtung: _getch() ist nicht im Standard und 
+//          daher abhängig vom Compiler
 #include "Labyrinth.hpp"
+
+using namespace std;
+
+// Konstruktor
+Labyrinth::Labyrinth() {
+	
+	// Größe des Labyrinths
+	const int kZeilen = 11;
+	const int kSpalten = 11;
+
+	// Zeichen, die im Labyrinth vorkommen können,
+	// NL = new line, EOS = end of string
+	enum Symbole { MAUER = '#', WEG = ' ', MUENZE = ':', NL = '\n', 
+	           EOS = '\0', ICH = 'X', GEIST = 'G' };
+
+	// Das Labyrinth als char-Array
+	// Die vorletzte Spalte speichert ein Zeilenendezeichen \n
+	// und die letzte ein \0-Zeichen, damit die Ausgabe leichter wird.
+	char labyrinth[kZeilen][kSpalten + 2];
+	
+}
+
+// Labyrinth mit # füllen
+void Labyrinth::initialisieren() {
+	for (int i = 1; i < kZeilen; i++) {
+		for (int j = 1; j < kSpalten; j++) {
+
+			labyrinth[i][j] = '#';
+
+		}
+		labyrinth[i][kSpalten + 1] = '\n';
+		labyrinth[i][kSpalten + 2] = '\0';
+	}
+}
+
+// Labyrinth auf dem Bildschirm ausgeben
+void Labyrinth::drucken() {
+	// Console frei machen
+	system("cls");
+	// Labyrinth ausgeben
+	for (int i = 1; i < kZeilen; i++) {
+		for (int j = 1; j < (kSpalten + 2); j++) {
+
+			cout << labyrinth[i][j];
+
+		}
+	}
+}

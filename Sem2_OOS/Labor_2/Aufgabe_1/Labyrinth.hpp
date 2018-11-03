4fefd62ef1f5788e2626b0da9008743f46a11e90
42ow0rm <38980815+42ow0rm@users.noreply.github.com>
2018-11-03 13:32:33 +0100

    Update Labyrinth.hpp

--- Sem2_OOS/Labor_2/Aufgabe_1/Labyrinth.hpp
+++ Sem2_OOS/Labor_2/Aufgabe_1/Labyrinth.hpp
@@ -1,1 +1,26 @@
 #
+
+class Labyrinth{
+	
+	// Größe des Labyrinths
+	const int kZeilen1;
+	const int kSpalten;
+
+	// Zeichen, die im Labyrinth vorkommen können,
+	// NL = new line, EOS = end of string
+	enum Symbole;
+
+	// Das Labyrinth als char-Array
+	// Die vorletzte Spalte speichert ein Zeilenendezeichen \n
+	// und die letzte ein \0-Zeichen, damit die Ausgabe leichter wird.
+	char labyrinth[kZeilen][kSpalten + 2];
+
+public:
+	// Konstruktor	
+	Labyrinth();
+	
+	void initialisieren();
+	
+	void drucken();
+		
+};

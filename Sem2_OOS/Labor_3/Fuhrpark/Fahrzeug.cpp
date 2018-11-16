77036ac44cf38eda666345fe13fc70cc176b276d
42ow0rm <38980815+42ow0rm@users.noreply.github.com>
2018-11-16 23:16:24 +0100

    Add files via upload

---
+++ Sem2_OOS/Labor_3/Fuhrpark/Fahrzeug.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include "Fahrzeug.hpp"
+#include "MyString.hpp"
+
+using namespace std;
+
+int Fahrzeug::count = 0;
+double Fahrzeug::summeKm = 0;
+
+Fahrzeug::Fahrzeug(){
+	count++;
+	this->km = 0;	
+	this->vin = count;
+}
+
+Fahrzeug::Fahrzeug(const Fahrzeug& f){
+	count++;
+	this->km = 0;
+	this->vin = count;
+}
+
+Fahrzeug::Fahrzeug(MyString &kenz){
+	count++;
+	this->km = 0;	
+	this->vin = count;
+}
+
+Fahrzeug::Fahrzeug(char *str){
+	count++;
+	MyString kenz(str);
+	kz.assign(kenz);
+	this->km = 0;
+	//cout << count << endl;
+	this->vin = count;
+}
+
+Fahrzeug::~Fahrzeug(){
+	count--;
+	summeKm -= km;
+}
+		
+int Fahrzeug::getCount(){
+	return count;
+}
+	
+void Fahrzeug::setKenz(MyString &kenz){
+	this->kz.assign(kenz);
+}
+
+const char* Fahrzeug::getKenz(){
+	return this->kz.c_str();
+}
+
+void Fahrzeug::fahren(double d){
+	this->km += d;
+	summeKm += d; 
+}
+
+double Fahrzeug::getKm(){
+	return this->km;
+}
+
+unsigned int Fahrzeug::getVIN(){
+	return this->vin;
+}
+
+double Fahrzeug::getSummeKm(){
+	return summeKm;
+}
+
+ostream& operator<< (ostream & s, Fahrzeug& f){
+	return s << f.getKenz() << " VIN = " << f.getVIN() << " km = " << f.getKm();
+}
+
